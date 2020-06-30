83cb3b7bc237b15f80d4a05edf5c1f31a1ba79a1
Evandro Falleiros <evandro.falleiros@ifms.edu.br>
2020-06-30 12:10:35 -0400

    correcoes sw

--- src/components/ServiceWorker.js
+++ src/components/ServiceWorker.js
@@ -29,7 +29,7 @@ function ServiceWorker() {
                   
                       window.addEventListener('load', () => {
                         // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-                        const swUrl = `/service-worker.js`;
+                        const swUrl = `/sw.js`;                      
                   
                         if (isLocalhost) {
                           // This is running on localhost. Let's check if a service worker still exists or not.
