35b2ab9b8e9b8fa1eadf3382cab0a46e837d1d82
Evandro Falleiros <evandro.falleiros@ifms.edu.br>
2020-11-23 16:52:15 -0400

    correcao dos links das imagens nos docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,9 @@ module.exports = {
   title: 'Embrapa',
   tagline: 'Agropecuaria Oeste',
   url: 'https://www.cpao.embrapa.br/cartilha/',
-  baseUrl: 'https://www.cpao.embrapa.br/cartilha/',
+  baseUrl: '/',
+  // url: 'https://www.cpao.embrapa.br/cartilha/',
+  // baseUrl: 'https://www.cpao.embrapa.br/cartilha/',
   favicon: 'img/logo.svg',
   organizationName: 'IFMS - campus Dourados / Embrapa', // Usually your GitHub org/user name.
   projectName: 'Tecnologias para a Agricultura Familiar Digital', // Usually your repo name.
